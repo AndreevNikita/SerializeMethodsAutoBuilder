eb251b5f59941e48d0ee8456d57da717caf3870d
Nikita Andreev <nickandreevart@gmail.com>
2020-12-18 01:05:08 +0300

    v1.0

---
+++ SerializeMethodsAutoBuilder/SerializeMethodsAutoBuilder/SMAB.cs
@@ -0,0 +1,111 @@
+﻿using InSharp;
+using NetBinSerializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeMethodsAutoBuilder {
+
+
+	public static class SMAB {
+		public delegate void Test(object obj, SerializeStream stream);
+		public static SerializeMethods BuildSerializeMethods(Type type, bool withComments = false) { 
+			var serializeGen = new ILGen<Action<object, SerializeStream>>(type + "_serialize", true);
+			var deserializeGen = new ILGen<Func<SerializeStream, object>>(type + "_deserialize", true);
+			ILVar serializeObject = serializeGen.DeclareVar(type);
+			ILVar deserializeObject = deserializeGen.DeclareVar(type);
+
+			serializeGen.Line(serializeObject.Set(serializeGen.args[0]));
+
+			EmitCodeRecursive(serializeGen, deserializeGen, serializeObject, deserializeObject, new Stack<FieldInfo>(), type);
+			
+			deserializeGen.Return(deserializeObject);
+
+			try {
+				var serializeFunc = serializeGen.compile(withComments);
+				var deserializeFunc = deserializeGen.compile(withComments);
+				return new SerializeMethods(new SerializeMethods.SerializeMethod(serializeFunc), new SerializeMethods.DeserializeMethod(deserializeFunc));
+			} catch(Exception e) { 
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+			}
+			return null;
+		}
+
+
+
+		public static void EmitCodeRecursive(ILGen serializeGen, ILGen deserializeGen, ILVar serializeObject, ILVar deserializeObject, Stack<FieldInfo> fieldsStack, Type currentType) { 
+			
+			//Get the current field path path
+			ILAssignable serializeFieldsPath = serializeObject;
+			ILAssignable deserializeFieldsPath = deserializeObject;
+			foreach(FieldInfo field in fieldsStack.Reverse()) {
+				serializeFieldsPath = serializeFieldsPath.Field(field);
+				deserializeFieldsPath = deserializeFieldsPath.Field(field);
+			}
+
+			RWMethodsInfo rwMethods = default(RWMethodsInfo);
+			bool castToType = false;
+			Type interfaceTypeBuffer = null;
+
+			if(SerializeStream.getBaseTypeRWMethodsIfExists(currentType, out rwMethods)) { 
+			} else if(typeof(Serializable).IsAssignableFrom(currentType)) { 
+				rwMethods = SerializeStream.rwSerializableMethodsInfo;
+				castToType = true;
+			} else if(currentType.IsArray) {
+				rwMethods = new RWMethodsInfo(
+					SerializeStream.rwArrayMethodsInfo.readMethodInfo.MakeGenericMethod(currentType),
+					SerializeStream.rwArrayMethodsInfo.writeMethodInfo.MakeGenericMethod(currentType)
+				);
+				castToType = true;
+			} else if(currentType.GetInterfaces().Any((Type checkInterface) => { 
+					if(checkInterface.IsGenericType && checkInterface.GetGenericTypeDefinition() == typeof(ICollection<>))  { 
+						interfaceTypeBuffer = checkInterface;  
+						return true; 
+					}
+					return false;
+				})) { 
+				Type elementType = interfaceTypeBuffer.GetGenericArguments()[0];
+				if(elementType.IsGenericType && elementType.GetGenericTypeDefinition() == typeof(KeyValuePair<,>)) {
+					Type[] genericArgs = elementType.GetGenericArguments();
+					rwMethods = new RWMethodsInfo(
+						SerializeStream.rwKeyValuePairsCollectionMethodsInfo.readMethodInfo.MakeGenericMethod(currentType, genericArgs[0], genericArgs[1]),
+						SerializeStream.rwKeyValuePairsCollectionMethodsInfo.writeMethodInfo.MakeGenericMethod(genericArgs[0], genericArgs[1])
+					);
+
+				} else { 
+					rwMethods = new RWMethodsInfo(
+						SerializeStream.rwCollectionMethodsInfo.readMethodInfo.MakeGenericMethod(currentType, elementType),
+						SerializeStream.rwCollectionMethodsInfo.writeMethodInfo.MakeGenericMethod(elementType)
+					);
+				}
+			} else {
+				
+				deserializeGen.Line(deserializeFieldsPath.Set(Expr.CreateUninitialized(currentType)));
+
+				foreach(FieldInfo fieldInfo in currentType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)) { 
+					Type fieldType = fieldInfo.FieldType;
+					fieldsStack.Push(fieldInfo);
+
+
+					EmitCodeRecursive(serializeGen, deserializeGen, serializeObject, deserializeObject, fieldsStack, fieldType);
+
+					fieldsStack.Pop();
+				}
+				return;
+			}
+
+			Expr serializeExpr = serializeFieldsPath;
+			ILAssignable deserializeExpr = deserializeFieldsPath;
+
+			serializeGen.Line(serializeGen.args[1].CallMethod(rwMethods.writeMethodInfo, serializeExpr));
+			deserializeGen.Line(deserializeExpr.Set(castToType ? deserializeGen.args[0].CallMethod(rwMethods.readMethodInfo).Cast(currentType) : deserializeGen.args[0].CallMethod(rwMethods.readMethodInfo)));
+				
+		}
+
+
+	}
+}
---
+++ SerializeMethodsAutoBuilder/Test/Program.cs
@@ -0,0 +1,91 @@
+﻿using NetBinSerializer;
+using SerializeMethodsAutoBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+
+
+	struct Vector {
+		public int x, y, z;
+
+		public override string ToString() {
+			return $"({x}; {y}; {z})";
+		}
+	}
+
+	class MyObject { 
+
+		public Vector Pos { get; set; }
+
+		public string Name { get; set; }
+		public List<string> Hobbies { get; set;}
+		public Dictionary<string, Dictionary<string, string>> KnownWords { get; private set; }
+		
+		public MyObject(Vector pos, string name, params string[] hobbies) {
+			this.Pos = pos;
+			this.Name = name;
+			this.Hobbies = new List<string>(hobbies);
+			KnownWords = new Dictionary<string, Dictionary<string, string>>(); 
+		}
+
+		public void sayHello() {
+			Console.WriteLine($"Hello! My name is {Name}");
+			Console.WriteLine($"My pos: {Pos}");
+			Console.Write("I like ");
+			for(int index = 0; index < Hobbies.Count; index++)
+				Console.Write(index == 0 ? $"{Hobbies[index]}" : (index != Hobbies.Count - 1 ? $", {Hobbies[index]}" : $" and {Hobbies[index]}\n"));
+			Console.WriteLine("I was translated by bytes array! It means, that I can be translated by network");
+
+			
+			Random rand = new Random();
+			string[] languagesArray = KnownWords.Keys.ToArray();
+			if(languagesArray.Length == 0) { 
+				Console.WriteLine("I don't know any other languages");
+			} else {
+				string lang = languagesArray[rand.Next(languagesArray.Length)];
+				Console.WriteLine("I know, that");
+				foreach(KeyValuePair<string, string> wordsPair in KnownWords[lang])
+					Console.WriteLine($"{wordsPair.Key} is {wordsPair.Value}");
+				Console.WriteLine($"on {lang} language");
+			}
+		}
+	}
+
+	class Program {
+		static void Main(string[] args) {
+			SerializeMethods methods = SMAB.BuildSerializeMethods(typeof(MyObject));
+
+			MyObject writeObject = new MyObject(new Vector { x = 4, y = 3, z = 5 }, "Tom", "to play in computer games", "eat testy food", "code");
+			writeObject.KnownWords["russian"] = new Dictionary<string, string> { 
+				{ "cat", "кот" },
+				{ "language", "язык" },
+				{ "mouse", "мышь" },
+				{ "code", "код" },
+			};
+			
+			SerializeStream sstream = new SerializeStream();
+			methods.serializeMethod(writeObject, sstream);
+
+			Console.WriteLine();
+			SerializeStream dstream = new SerializeStream(sstream.getBytes());
+			((MyObject)methods.deserializeMethod(dstream)).sayHello();
+			Console.ReadKey();
+		}
+
+		public static object readVector(SerializeStream ds) { 
+			object obj;
+			Vector vec = (Vector)FormatterServices.GetUninitializedObject(typeof(Vector));
+			vec.x = ds.readInt32();
+			vec.y = ds.readInt32();
+			vec.z = ds.readInt32();
+			obj = vec;
+			return obj;
+		}
+
+	}
+}
